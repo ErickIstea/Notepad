b228d2ab9dca6ad3bafc5e84103b90ecac2d64c1
ErickIstea <erick.silva@istea.com.ar>
2024-04-23 20:19:55 -0300

    Agrego boton flotante

--- app/src/main/java/com/istea/notepad/pages/MainPage.kt
+++ app/src/main/java/com/istea/notepad/pages/MainPage.kt
@@ -1,7 +1,12 @@
 package com.istea.notepad.pages
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -26,7 +31,9 @@ fun MainPage(modifier: Modifier = Modifier) {
 
     Scaffold(
         modifier = modifier,
-        topBar = { MainTopAppBar() }
+        topBar = { MainTopAppBar() },
+        floatingActionButton = { BotonCrear(navHostController) }
+
     ) {
         MainNavHost(
             modifier = Modifier.padding(it),
@@ -55,6 +62,13 @@ fun MainNavHost(
         }
         composable("detalle") { DetallePage() }
         composable("crearNota") { CrearNotaPage() }
+    }
+}
+
+@Composable
+fun BotonCrear(navHostController: NavHostController ){
+    FloatingActionButton(onClick = { navHostController.navigate("crearNota")}) {
+        Icon(imageVector = Icons.Filled.Add, contentDescription = "nueva nota")
     }
 }
 

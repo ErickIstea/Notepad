e0580da924234bfb133af2c9a1d003f908b88f62
ErickIstea <erick.silva@istea.com.ar>
2024-04-23 20:42:02 -0300

    Agrego card clickeable para los items de la lista

--- app/src/main/java/com/istea/notepad/pages/ListaPage.kt
+++ app/src/main/java/com/istea/notepad/pages/ListaPage.kt
@@ -1,13 +1,18 @@
 package com.istea.notepad.pages
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.istea.notepad.ui.theme.NotepadTheme
 
 @Composable
@@ -18,7 +23,16 @@ fun ListaPage(
 ) {
     LazyColumn(modifier = modifier){
         items(items = notas) {
-            Text(text = it)
+            Card (
+                modifier = Modifier
+                    .padding(10.dp)
+                    .clickable { onNotaSelected() }
+            ){
+                Text(
+                    modifier = Modifier.padding(10.dp),
+                    text = it
+                )
+            }
         }
     }
 
@@ -27,7 +41,7 @@ fun ListaPage(
 @Preview(showBackground = true)
 @Composable
 fun ListaPagePreview() {
-    val notas = remember { mutableStateListOf<String>() }
+    val notas = remember { mutableStateListOf<String>("jojo","lala","hola") }
     NotepadTheme {
         ListaPage(
             onNotaSelected = {},
